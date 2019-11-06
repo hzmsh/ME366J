4c8e3c7a62404a20eda9e84925119496ded34293
hzmsh <35788891+hzmsh@users.noreply.github.com>
2019-11-06 11:46:42 -0600

    Update printer_object.py

--- printer_object.py
+++ printer_object.py
@@ -1,7 +1,5 @@
 import math 
 
-
-
 class printer():
 	def __init__(self, pulley_radius,screw_pitch):
     	self.pulley_radius = pulley_radius
@@ -20,21 +18,22 @@ class printer():
 		return (screw_angle)
 
 	# c = [r,theta,E], this method will determine the dist we input into get_screw_angle
-	# bonus: figure out how to use the midpoint to calculate more accurate dist 
 	def get_E_dist(self, c_old, c):
 		r1 = c_old[0]
 		theta1 = c_old[1]
 		r2 = c[0]
 		theta2 = c[1]
+
 		# convert r,theta to x,y
-		x1 = r1*(math.sin(theta1))
-		y1 = r1*(math.cos(theta1))
-		x2 = r2*(math.sin(theta2))
-		y2 = r2*(math.cos(theta2))	
+		x1 = r1*(math.cos(theta1))
+		y1 = r1*(math.sin(theta1))
+		x2 = r2*(math.cos(theta2))
+		y2 = r2*(math.sin(theta2))	
 
 		# midpoint coord
-		# consider three senarios, both posi, both nega, one posi + one nega
+		# consider three senarios, x1,x2 both posi, both nega, one posi + one nega
  		x3 = abs(x1-x2)/2 + min(x1,x2)
+  		y3 = abs(y1-y2)/2 + min(y1,y2)
 
 		#compute dist
 		dist1 = math.sqrt((x1-x3)^2+(y1-y3)^2)
@@ -43,5 +42,4 @@ class printer():
 
 		# call get_screw_angle
 		screw_angle = self.get_screw_angle(dist)
-		return (screw_angle)
-
+		return (screw_angle)                  
